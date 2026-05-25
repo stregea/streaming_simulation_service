4a6462b8d4708b5280bf940527a8a97997208a59
Sam Tregea <sdtregea@gmail.com>
2026-05-25 12:09:25 -0400

    Add initial files from the Spring Initializr

---
+++ src/test/java/com/streaming/simulation_service/SimulationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.simulation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
