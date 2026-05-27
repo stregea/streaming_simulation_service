1336ab06530faf8e7de6d855cfaf0603d0f0b949
Sam Tregea <sdtregea@gmail.com>
2026-05-27 08:36:23 -0400

    Add Simulator interface for sport-specific game event simulation

---
+++ src/main/java/com/streaming/simulation_service/simulators/Simulator.java
@@ -0,0 +1,37 @@
+package com.streaming.simulation_service.simulators;
+
+import com.streaming.simulation_service.model.event.EventType;
+import com.streaming.simulation_service.model.event.GameEvent;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Defines the contract for sport-specific game event simulators.
+ *
+ * @see GameEvent
+ * @see EventType
+ */
+public interface Simulator {
+
+    /**
+     * Generate a new game event based on the current sport being played.
+     *
+     * @return a {@link GameEvent} object containing new event information.
+     */
+    public GameEvent generateEvent();
+
+    /**
+     * Generate a list of {@link EventType} for the sport to be inserted within a {@link GameEvent}.
+     *
+     * @return a list of {@link EventType}.
+     */
+    public List<EventType> getEventTypes();
+
+    /**
+     * Generate a simulated payload to be inserted within a {@link GameEvent}.
+     *
+     * @return a Map containing sports data.
+     */
+    public Map<String, Object> generatePayload();
+}
