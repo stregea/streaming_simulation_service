a4a32eb1490adbf72c20983a17275e6ebc2046f5
Sam Tregea <sdtregea@gmail.com>
2026-05-27 08:35:16 -0400

    Add enums for EventType and Sport

---
+++ src/main/java/com/streaming/simulation_service/model/enums/Sport.java
@@ -0,0 +1,12 @@
+package com.streaming.simulation_service.model.enums;
+
+/**
+ * Enum that will represent the different sports that are simulated.
+ */
+public enum Sport {
+    SOCCER,
+    BJJ,
+    FOOTBALL,
+    BASKETBALL,
+    BASEBALL
+}
---
+++ src/main/java/com/streaming/simulation_service/model/event/EventType.java
@@ -0,0 +1,12 @@
+package com.streaming.simulation_service.model.event;
+
+/**
+ * Enum that will represent the type of event that occurred.
+ */
+public enum EventType {
+    PASS, // soccer
+    SHOT, //soccer
+    GOAL, // soccer
+    MISS, // soccer
+    ASSIST // soccer
+}
