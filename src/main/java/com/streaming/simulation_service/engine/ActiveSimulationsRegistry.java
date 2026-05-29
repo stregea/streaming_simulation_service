699f9689bf09cf431707e0de1b47beddcdc63c66
Sam Tregea <sdtregea@gmail.com>
2026-05-29 14:14:00 -0400

    Move ActiveSimulationsRegistry back to the engine directory

--- src/main/java/com/streaming/simulation_service/engine/ActiveSimulationsRegistry.java
+++ src/main/java/com/streaming/simulation_service/engine/ActiveSimulationsRegistry.java
@@ -1,4 +1,4 @@
-package com.streaming.simulation_service.registry;
+package com.streaming.simulation_service.engine;
 
 import com.streaming.simulation_service.model.state.SimulationGameState;
 import com.streaming.simulation_service.model.game.Game;
