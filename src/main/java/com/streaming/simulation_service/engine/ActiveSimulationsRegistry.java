9c04d4a37913a33a04c96d634bfa714fc49ed0b7
Sam Tregea <sdtregea@gmail.com>
2026-05-29 12:53:18 -0400

    Rename ActiveGamesRegistry to ActiveSimulationsRegistry

--- src/main/java/com/streaming/simulation_service/engine/ActiveSimulationsRegistry.java
+++ src/main/java/com/streaming/simulation_service/engine/ActiveSimulationsRegistry.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-public class ActiveGamesRegistry {
+public class ActiveSimulationsRegistry {
 
     /**
      * The ConcurrentHashMap that will serve as the primary registry of all active games.
