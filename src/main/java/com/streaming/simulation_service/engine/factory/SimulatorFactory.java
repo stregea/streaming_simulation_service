1c441791bde2321b811995727205e6794e43593a
Sam Tregea <sdtregea@gmail.com>
2026-06-09 17:57:51 -0400

    Refactor SimulatorFactory package structure and update imports in SimulationEngine

--- src/main/java/com/streaming/simulation_service/engine/factory/SimulatorFactory.java
+++ src/main/java/com/streaming/simulation_service/engine/factory/SimulatorFactory.java
@@ -1,4 +1,4 @@
-package com.streaming.simulation_service.engine;
+package com.streaming.simulation_service.engine.factory;
 
 import com.streaming.simulation_service.model.enums.Sport;
 import com.streaming.simulation_service.simulators.Simulator;
